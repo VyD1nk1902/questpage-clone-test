65530eb93c8a900b53c03b4403048aad8e700236
VyDink <vy.dinh.2929@gmail.com>
2025-10-01 21:04:21 +0700

    Update sidebar

---
+++ src/components/SideBar/AppSidebar.tsx
@@ -0,0 +1,129 @@
+import LuckyTech_logo from "/public/Sidebar/LuckyTech_Logo.png";
+import LuckyTech_mini from "/public/Sidebar/LuckyTech_mini_logo.svg";
+
+import * as React from "react";
+import {
+  AudioWaveform,
+  BookOpen,
+  Bot,
+  Command,
+  CompassIcon,
+  Frame,
+  GalleryVerticalEnd,
+  Headset,
+  Map,
+  PieChart,
+  Settings2,
+  SquareTerminal,
+  TrophyIcon,
+  UserCircleIcon,
+} from "lucide-react";
+
+import { NavMain } from "./nav-main";
+import { NavProjects } from "./nav-project";
+import { NavUser } from "./nav-users";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar";
+import { GearIcon, UsersThreeIcon } from "@phosphor-icons/react";
+import { Separator } from "../ui/separator";
+
+// This is sample data.
+const data = {
+  user: {
+    name: "CryptoHawk",
+    code: "0x76x..ea92",
+    avatar: "https://github.com/shadcn.png",
+  },
+  teams: [
+    {
+      name: "Acme Inc",
+      logo: GalleryVerticalEnd,
+      plan: "Enterprise",
+    },
+    {
+      name: "Acme Corp.",
+      logo: AudioWaveform,
+      plan: "Startup",
+    },
+    {
+      name: "Evil Corp.",
+      logo: Command,
+      plan: "Free",
+    },
+  ],
+  navMain: [
+    {
+      title: "Discovery & Earn",
+      url: "#",
+      icon: CompassIcon,
+    },
+    {
+      title: "Leaderboard",
+      url: "#",
+      icon: TrophyIcon,
+    },
+    {
+      title: "Communities",
+      url: "#",
+      icon: UsersThreeIcon,
+    },
+    {
+      title: "Profile",
+      url: "#",
+      icon: UserCircleIcon,
+    },
+  ],
+  projects: [
+    {
+      name: "Setting",
+      url: "#",
+      icon: GearIcon,
+    },
+    {
+      name: "Support",
+      url: "#",
+      icon: Headset,
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar
+      collapsible="icon"
+      {...props}
+      // style={{ "--sidebar-width": "260px" } as React.CSSProperties}
+      className="border border-r border-border bg-sidebar"
+    >
+      <SidebarHeader className="flex flex-col group-data-[collapsible=icon]:place-items-center">
+        <a href="#">
+          <img
+            src={LuckyTech_logo}
+            className="w-full object-contain h-full group-data-[collapsible=icon]:hidden"
+            alt="LuckyTech_logo"
+          />
+          <img
+            src={LuckyTech_mini}
+            className="hidden w-7 object-contain h-7 group-data-[collapsible=icon]:block"
+            alt="LuckyTech_logo"
+          />
+        </a>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+        <Separator />
+        <NavProjects projects={data.projects} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
---
+++ src/components/SideBar/nav-users.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import {
+  BadgeCheck,
+  Bell,
+  Check,
+  ChevronsUpDown,
+  CreditCard,
+  LogOut,
+  Plus,
+  Sparkles,
+} from "lucide-react";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "@/components/ui/sidebar";
+import { Separator } from "../ui/separator";
+
+export function NavUser({
+  user,
+}: {
+  user: {
+    name: string;
+    code: string;
+    avatar: string;
+  };
+}) {
+  const { isMobile } = useSidebar();
+
+  return (
+    <SidebarMenu>
+      <SidebarMenuItem>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <SidebarMenuButton
+              size="lg"
+              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+            >
+              <Avatar>
+                <AvatarImage src={user.avatar} alt={user.name} />
+                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+              </Avatar>
+              <div className="grid flex-1 text-left text-sm leading-tight">
+                <span className="truncate font-medium">{user.name}</span>
+                <span className="truncate text-xs">{user.code}</span>
+              </div>
+              <ChevronsUpDown className="ml-auto size-4" />
+            </SidebarMenuButton>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-[10px] p-2"
+            side={isMobile ? "bottom" : "right"}
+            align="end"
+            sideOffset={4}
+          >
+            <DropdownMenuLabel className="p-0 font-normal">
+              <DropdownMenuItem>Switch Account</DropdownMenuItem>
+            </DropdownMenuLabel>
+            <Separator className="my-2" />
+            <DropdownMenuGroup>
+              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                <Avatar>
+                  <AvatarImage src={user.avatar} alt={user.name} />
+                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                </Avatar>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-medium">{user.name}</span>
+                  <span className="truncate text-xs">{user.code}</span>
+                </div>
+                <Check size={20} />
+              </div>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <Plus />
+              AddAccount
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </SidebarMenuItem>
+    </SidebarMenu>
+  );
+}
