a8cbc25f26405a2a766fe98d39ffb65a8a397f60
VyDink <vy.dinh.2929@gmail.com>
2025-10-01 21:05:04 +0700

    Update sidebar

--- src/modules/MainLayout/Header.tsx
+++ src/modules/MainLayout/Header.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@/components/ui/button";
 import useDeviceType from "@/hooks/useMediaQuery";
 import { cn } from "@/lib/utils";
 import { useEffect } from "react";
@@ -20,6 +19,9 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const Header = () => {
   const deviceType = useDeviceType();
@@ -31,9 +33,42 @@ const Header = () => {
   return (
     <div
       className={cn(
-        "w-full max-w-screen py-3 flex fixed top-0 left-0 right-0 z-50 bg-background shrink-0 items-center gap-2 border-b h-[65px]"
+        "w-full max-w-screen py-3 flex sticky top-0 z-50 bg-accent shrink-0 items-center gap-2 border-b h-[65px]",
+        "px-3 flex gap-3 justify-between"
       )}
-    ></div>
+    >
+      <div className="flex gap-6 justify-center items-center">
+        <SidebarTrigger />
+        <Button className="w-full h-[36px] bg-input !rounded-[6px] border border-white flex gap-1">
+          <input
+            type="search"
+            placeholder="Search"
+            className="w-[73px] bg-transparent focus:outline-none"
+          />
+          <Button className="bg-transparent">Press</Button>
+        </Button>
+      </div>
+      <div className="flex items-center gap-3">
+        <div className="flex gap-2">
+          <img
+            src="/public/Mission/diamond-logo.png"
+            width={"28px"}
+            height={"28px"}
+            className="object-contain"
+            alt=""
+          />
+          <div className="flex flex-col">
+            <span className="text-muted-foreground">Loyalty</span>
+            <span className="text-sm font-medium">1000 Point</span>
+          </div>
+        </div>
+        <img src="/public/Line (Stroke).svg" className="mx-1" alt="" />
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </div>
+    </div>
   );
 };
 
--- src/modules/MainLayout/index.tsx
+++ src/modules/MainLayout/index.tsx
@@ -5,24 +5,43 @@ import Footer from "./Footer";
 import { cn } from "classnames-merge-tw";
 import { useLocation, useParams } from "react-router-dom";
 
+import { AppSidebar } from "@/components/SideBar/AppSidebar";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+
 const Index = ({ children }: { children: React.ReactNode }) => {
   const deviceType = useDeviceType();
 
   return (
-    <>
-      <Header />
-      <main
-        className={cn(
-          "flex-1 pb-6 flex flex-col bg-background",
-          deviceType == "desktop"
-            ? "w-[70%] max-w-5xl mx-auto pt-[80px] min-w-[224px]"
-            : "w-full pt-[72px]"
-        )}
-      >
-        {children}
-      </main>
-      <Footer />
-    </>
+    <SidebarProvider>
+      <AppSidebar side="left" variant="sidebar" />
+      <SidebarInset>
+        <Header />
+        <main
+          className={cn(
+            "flex-1 pb-6 flex flex-col bg-background",
+            deviceType == "desktop"
+              ? "w-[70%] h-screen max-w-5xl mx-auto pt-[80px] min-w-[224px]"
+              : "w-full pt-[72px]"
+          )}
+        >
+          {children}
+        </main>
+        <Footer />
+      </SidebarInset>
+    </SidebarProvider>
   );
 };
 
