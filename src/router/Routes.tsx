2a641593912b7ae347785c1ca18765f47a1b140d
VyDink <vy.dinh.2929@gmail.com>
2025-10-08 16:35:28 +0700

    Add path for profile page

--- src/router/Routes.tsx
+++ src/router/Routes.tsx
@@ -30,6 +30,11 @@ export const routes: Route[] = [
   },
 
   {
+    path: "/profile",
+    Component: lazy(() => import("@/pages/Profile")),
+  },
+
+  {
     path: "/leaderboard",
     Component: lazy(() => import("@/pages/LeaderBoardPage")),
   },
