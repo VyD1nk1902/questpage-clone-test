46afbd323a7450f4e8965c80ba47c16d8932d4a8
VyDink <vy.dinh.2929@gmail.com>
2025-10-16 17:29:04 +0700

    Responsive

--- src/pages/Community.tsx
+++ src/pages/Community.tsx
@@ -12,10 +12,12 @@ import {
   bg_Tele,
   bg_X,
 } from "@/constants/image.constant";
+import { CardContent } from "@/components/ui/card";
 
 import { Link } from "react-router-dom";
 import { Separator } from "@/components/ui/separator";
 import { UserIcon } from "@phosphor-icons/react";
+import useDeviceType from "@/hooks/useMediaQuery";
 
 const data = [
   {
@@ -45,21 +47,62 @@ const data = [
 ];
 
 const Community = () => {
+  const deviceType = useDeviceType();
   return (
     <div className="main-content !px-0 max-w-2xl mx-auto">
       <div className="flex flex-col gap-6">
         <span className="px-6 text-xl sm:text-3xl font-semibold">
           Our official community
         </span>
-        <Carousel className="w-full relative" opts={{ loop: true }}>
-          <CarouselContent>
+        {deviceType == "desktop" ? (
+          <Carousel className="w-full relative" opts={{ loop: true }}>
+            <CarouselContent>
+              {data.map((item) => (
+                <CarouselItem
+                  key={item.id}
+                  className="sm:basis-1/2 md:basis-1/3 w-full"
+                >
+                  <Link to="#">
+                    <div className="w-full h-full flex flex-col items-center gap-1 flex-grow flex-shrink-0 basis-0 rounded-2xl bg-[linear-gradient(180deg,var(--background)_0%,var(--accent)_100%)] border border-border overflow-hidden">
+                      <div className="relative w-full aspect-square">
+                        <img
+                          src={item.background}
+                          className="w-full h-full rounded-t-2xl object-cover"
+                          alt="background-carousel"
+                        />
+                        <div
+                          className="absolute inset-x-0 bottom-0 h-1/2 z-10"
+                          style={{
+                            background:
+                              "linear-gradient(180deg, rgba(0, 0, 0, 0)50%, var(--accent) 100%)",
+                          }}
+                        ></div>
+                      </div>
+
+                      <div className="w-full flex p-4 gap-2 flex-col ">
+                        <span className="text-base font-semibold">
+                          {item.title}
+                        </span>
+                        <Separator />
+                        <span className="flex gap-1">
+                          <UserIcon size={16} />
+                          {item.desc}
+                        </span>
+                      </div>
+                    </div>
+                  </Link>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious className="!absolute left-3" />
+            <CarouselNext className="!absolute right-3" />
+          </Carousel>
+        ) : (
+          <CardContent className="grid grid-cols-2 gap-2">
             {data.map((item) => (
-              <CarouselItem
-                key={item.id}
-                className="sm:basis-1/2 md:basis-1/3 w-full"
-              >
+              <div key={item.id} className="w-full">
                 <Link to="#">
-                  <div className="w-full h-full flex flex-col items-center gap-1 flex-grow flex-shrink-0 basis-0 rounded-2xl bg-[linear-gradient(180deg,var(--background)_0%,var(--accent)_100%)] border border-border overflow-hidden">
+                  <div className="w-full h-full flex flex-col  items-center gap-1 flex-grow flex-shrink-0 basis-0 rounded-2xl bg-[linear-gradient(180deg,var(--background)_0%,var(--accent)_100%)] border border-border overflow-hidden">
                     <div className="relative w-full aspect-square">
                       <img
                         src={item.background}
@@ -87,12 +130,10 @@ const Community = () => {
                     </div>
                   </div>
                 </Link>
-              </CarouselItem>
+              </div>
             ))}
-          </CarouselContent>
-          <CarouselPrevious className="!absolute left-3" />
-          <CarouselNext className="!absolute right-3" />
-        </Carousel>
+          </CardContent>
+        )}
       </div>
     </div>
   );
