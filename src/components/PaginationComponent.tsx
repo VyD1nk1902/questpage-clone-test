643680155336f789cfd5b4f7264a98a90e4bf3c4
VyDink <vy.dinh.2929@gmail.com>
2025-10-17 11:43:57 +0700

    Responsive mobile

--- src/components/PaginationComponent.tsx
+++ src/components/PaginationComponent.tsx
@@ -13,20 +13,20 @@ const PaginationComponent = (props: PaginationProps) => {
 
   return (
     <Pagination
-      className="ant-pagination !mt-10 max-[350px]:!mt-0"
+      className="ant-pagination !mt-2 max-[350px]:!mt-0"
       pageSize={10}
       onChange={setCurrentPage}
       align="center"
       current={currentPage}
       total={total}
       prevIcon={
-        <div className="h-7 w-fit px-3 flex gap-1 items-center select-none">
+        <div className="h-7 w-fit px-3 flex gap-1 items-center select-none max-[450px]:hidden">
           <CaretLeft className="w-4 h-4 object-cover text-foreground" />
           <span>Previous</span>
         </div>
       }
       nextIcon={
-        <div className="h-7 w-fit px-3 flex gap-1 items-center select-none">
+        <div className="h-7 w-fit px-3 flex gap-1 items-center select-none max-[450px]:hidden">
           <span>Next</span>
           <CaretRight className="w-4 h-4 object-cover text-foreground" />
         </div>
