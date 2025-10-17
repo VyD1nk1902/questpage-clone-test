a387964dbcb6a737874f165e166bf3151637a334
VyDink <vy.dinh.2929@gmail.com>
2025-10-17 15:32:33 +0700

    Responsive mobile

--- src/components/LeaderBoard.tsx
+++ src/components/LeaderBoard.tsx
@@ -27,114 +27,118 @@ const LeaderBoard = (props: LeaderBoardProps) => {
   const deviceType = useDeviceType();
   return (
     <div className={cn(!myRank && "lg:!min-h-[716px] lg:!h-[716px]")}>
-      <Table className="border-separate border-spacing-y-2">
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-14 text-center py-5 align-middle">
-              {myRank ? (
-                <span className="px-2 py-1 bg-secondary rounded border border-border text-lg">
-                  {myRank?.myRank || 1}
-                </span>
-              ) : (
-                <span className="p-2 text-sm">#</span>
-              )}
-            </TableHead>
-            <TableHead className="py-3 align-middle">
-              {myRank ? (
-                <div className="flex gap-3 items-center">
-                  <Avatar className="w-6 h-6 object-contain">
-                    <AvatarImage
-                      src={myRank?.avatar || "https://github.com/shadcn.png"}
-                      alt={myRank?.username || "username"}
-                    />
-                    <AvatarFallback className="rounded-lg">CN</AvatarFallback>
-                  </Avatar>
-                  <div className="flex flex-col gap-1">
-                    <span className="text-sm font-medium">
-                      {myRank?.username || "username"}
-                    </span>
-                    <span className="text-xs font-medium text-muted-foreground">
-                      {getShortAddress(myRank?.walletAddress || "")}
-                    </span>
-                  </div>
-                </div>
-              ) : (
-                <span className="text-sm">Wallet</span>
-              )}
-            </TableHead>
-            {levelColumn && deviceType == "desktop" && (
-              <TableHead className="p-2 w-16">
-                <span className="text-sm">LV</span>
-              </TableHead>
-            )}
-            <TableHead className="p-2 w-24">
-              {myRank ? (
-                <div className="flex gap-2 justify-start items-center text-base font-bold">
-                  <img
-                    src={DiamondLogo}
-                    className="w-4 h-4"
-                    alt="diamond-logo"
-                  />
-                  {myRank?.totalXp}
-                </div>
-              ) : (
-                <span className="text-sm">Total pts</span>
-              )}
-            </TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {data &&
-            data.map((item: any, index: number) => (
-              <TableRow key={index}>
-                <TableCell className="w-10 text-center">
+      <div className="overflow-x-auto">
+        <Table className="border-separate border-spacing-y-2">
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-14 text-center py-5 align-middle">
+                {myRank ? (
                   <span className="px-2 py-1 bg-secondary rounded border border-border text-lg">
-                    {index + 1 + (currentPage - 1) * 10}
+                    {myRank?.myRank || 1}
                   </span>
-                </TableCell>
-                <TableCell>
+                ) : (
+                  <span className="p-2 text-sm">#</span>
+                )}
+              </TableHead>
+              <TableHead className="py-3 align-middle">
+                {myRank ? (
                   <div className="flex gap-3 items-center">
                     <Avatar className="w-6 h-6 object-contain">
                       <AvatarImage
-                        src={item.avatar || "https://github.com/shadcn.png"}
-                        alt={item.username}
+                        src={myRank?.avatar || "https://github.com/shadcn.png"}
+                        alt={myRank?.username || "username"}
                       />
                       <AvatarFallback className="rounded-lg">CN</AvatarFallback>
                     </Avatar>
                     <div className="flex flex-col gap-1">
                       <span className="text-sm font-medium">
-                        {item.username}
+                        {myRank?.username || "username"}
                       </span>
                       <span className="text-xs font-medium text-muted-foreground">
-                        {getShortAddress(item?.walletAddress || "")}
+                        {getShortAddress(myRank?.walletAddress || "")}
                       </span>
                     </div>
                   </div>
-                </TableCell>
-                {levelColumn && deviceType == "desktop" && (
-                  <TableCell>
-                    <span>{item.level}</span>
-                  </TableCell>
+                ) : (
+                  <span className="text-sm">Wallet</span>
                 )}
-
-                <TableCell>
+              </TableHead>
+              {levelColumn && deviceType == "desktop" && (
+                <TableHead className="p-2 w-16">
+                  <span className="text-sm">LV</span>
+                </TableHead>
+              )}
+              <TableHead className="p-2 w-24">
+                {myRank ? (
                   <div className="flex gap-2 justify-start items-center text-base font-bold">
                     <img
                       src={DiamondLogo}
                       className="w-4 h-4"
                       alt="diamond-logo"
                     />
-                    {type == "daily"
-                      ? item.pointDuringTheDay
-                      : type == "week"
-                      ? item.pointDuringTheWeek
-                      : item.totalXp}
+                    {myRank?.totalXp}
                   </div>
-                </TableCell>
-              </TableRow>
-            ))}
-        </TableBody>
-      </Table>
+                ) : (
+                  <span className="text-sm">Total pts</span>
+                )}
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {data &&
+              data.map((item: any, index: number) => (
+                <TableRow key={index}>
+                  <TableCell className="w-10 text-center">
+                    <span className="px-2 py-1 bg-secondary rounded border border-border text-lg">
+                      {index + 1 + (currentPage - 1) * 10}
+                    </span>
+                  </TableCell>
+                  <TableCell>
+                    <div className="flex gap-3 items-center">
+                      <Avatar className="w-6 h-6 object-contain">
+                        <AvatarImage
+                          src={item.avatar || "https://github.com/shadcn.png"}
+                          alt={item.username}
+                        />
+                        <AvatarFallback className="rounded-lg">
+                          CN
+                        </AvatarFallback>
+                      </Avatar>
+                      <div className="flex flex-col gap-1">
+                        <span className="text-sm font-medium">
+                          {item.username}
+                        </span>
+                        <span className="text-xs font-medium text-muted-foreground">
+                          {getShortAddress(item?.walletAddress || "")}
+                        </span>
+                      </div>
+                    </div>
+                  </TableCell>
+                  {levelColumn && deviceType == "desktop" && (
+                    <TableCell>
+                      <span>{item.level}</span>
+                    </TableCell>
+                  )}
+
+                  <TableCell>
+                    <div className="flex gap-2 justify-start items-center text-base font-bold">
+                      <img
+                        src={DiamondLogo}
+                        className="w-4 h-4"
+                        alt="diamond-logo"
+                      />
+                      {type == "daily"
+                        ? item.pointDuringTheDay
+                        : type == "week"
+                        ? item.pointDuringTheWeek
+                        : item.totalXp}
+                    </div>
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </div>
     </div>
   );
 };
