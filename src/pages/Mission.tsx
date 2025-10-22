9d5b2bfeb625f02246dd29edb8fd5dc4b238361a
VyDink <vy.dinh.2929@gmail.com>
2025-10-22 13:24:59 +0700

    Add metaTags for slug

--- src/pages/Mission.tsx
+++ src/pages/Mission.tsx
@@ -1,14 +1,15 @@
 import React from "react";
-import HomePage from "./HomePage";
 import ArcherHunter from "@/modules/Mission/ArcherHunter";
 import { Separator } from "@/components/ui/separator";
 import ClaimReward from "@/modules/Mission/ClaimReward";
 import QuestTab from "@/modules/Mission/QuestTab";
 import QuestDetail from "@/modules/Mission/QuestDetail";
+import CampaignMeta from "@/components/meta/CampaignMeta";
 
 const Mission = () => {
   return (
     <div className="main-content max-w-5xl mx-auto min-w-[224px]">
+      <CampaignMeta />
       <ArcherHunter />
       <Separator />
       <ClaimReward />
