70d38bd332bd36c37305b694476bfadbe158ede6
VyDink <vy.dinh.2929@gmail.com>
2025-10-22 13:25:20 +0700

    Update using react-head for SEO

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "@/styles/global.css";
+import { HeadProvider } from "react-head";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <HeadProvider>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </HeadProvider>
 );
